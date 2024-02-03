10a023e96f06e75cd1dae0b7794b0f50e52adf0f
Muhammad Isaac Mohamed Khaizad <s10242151@connect.np.edu.sg>
2024-02-03 18:32:14 +0800

    Did the send msg thing BUT its not 100% linked yet

---
+++ WebApplication1/Controllers/SmsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System.Configuration;
+
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+using Twilio.TwiML;
+using Twilio.AspNet.Mvc;
+using Twilio.Rest.Microvisor.V1;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Twilio.Exceptions;
+
+
+namespace WebApplication1.Controllers
+{
+    public class SmsController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public SmsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public ActionResult SendSms()
+        {
+            var accountSid = _configuration["Twilio:AccountSid"];
+            var authToken = _configuration["Twilio:AuthToken"];
+            TwilioClient.Init(accountSid, authToken);
+
+            var to = new PhoneNumber(_configuration["Twilio:MyPhoneNumber"]);
+            var from = new PhoneNumber("+16317106309");
+            Console.WriteLine(to);
+            Console.WriteLine(from);
+
+            var message = MessageResource.Create(
+                to: to,
+                from: from,
+                body: "Bro Ur teammates are hella useless dawg i get it i feel u...");
+
+            return RedirectToAction("Withdraw" ,"Home");
+        }
+    }
+}
