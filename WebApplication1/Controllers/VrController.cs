ca4556411b7750acccf19586feda932e583d69ac
Nattt0101 <S10242410@connect.np.edu.sg>
2024-02-04 20:24:31 +0800

    errors

--- WebApplication1/Controllers/VrController.cs
+++ WebApplication1/Controllers/VrController.cs
@@ -56,7 +56,7 @@ namespace WebApplication1.Controllers
         }
 
 
-   
+
         public IActionResult Feedback()
         {
             return View();
@@ -75,47 +75,48 @@ namespace WebApplication1.Controllers
 
         public IActionResult Details()
         {
-            List<User> AccountDetails = GetDetailsForAccount1();
+            return View();
+            //List<User> AccountDetails = GetDetailsForAccount1();
 
-            return View(AccountDetails);
+            //return View(AccountDetails);
         }
 
 
-        private List<User> GetDetailsForAccount1()
-        {
-            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=OCBC;Integrated Security=True;";
+        //private List<User> GetDetailsForAccount1()
+        //{
+        //    string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=OCBC;Integrated Security=True;";
 
-            List<User> accountDetails = new List<User>();
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
+        //    List<User> accountDetails = new List<User>();
+        //    using (SqlConnection connection = new SqlConnection(connectionString))
+        //    {
+        //        connection.Open();
 
-                // Query account details specifically for AccountID 1
-                string query = "SELECT * FROM [OCBC].[dbo].[Account] WHERE [AccountID] = 1";
+        //        // Query account details specifically for AccountID 1
+        //        string query = "SELECT * FROM [OCBC].[dbo].[Account] WHERE [AccountID] = 1";
 
-                using (SqlCommand command = new SqlCommand(query, connection))
-                {
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            User user = new User
-                            {
-                                AccountID = Convert.ToInt32(reader["AccountID"]),
-                                AccountNumber = Convert.ToString(reader["AccountNumber"]),
-                                Password = Convert.ToString(reader["CardCSV"]), // Assuming CardCSV is the Password field
-                                Name = Convert.ToString(reader["Name"]),
-                                Amount = Convert.ToDecimal(reader["Amount"])
-                            };
+        //        using (SqlCommand command = new SqlCommand(query, connection))
+        //        {
+        //            using (SqlDataReader reader = command.ExecuteReader())
+        //            {
+        //                while (reader.Read())
+        //                {
+        //                    User user = new User
+        //                    {
+        //                        AccountID = Convert.ToInt32(reader["AccountID"]),
+        //                        AccountNumber = Convert.ToString(reader["AccountNumber"]),
+        //                        Password = Convert.ToString(reader["CardCSV"]), // Assuming CardCSV is the Password field
+        //                        Name = Convert.ToString(reader["Name"]),
+        //                        Amount = Convert.ToDecimal(reader["Amount"])
+        //                    };
 
-                            accountDetails.Add(user);
-                        }
-                    }
-                }
-            }
+        //                    accountDetails.Add(user);
+        //                }
+        //            }
+        //        }
+        //    }
 
-            return accountDetails;
-        }
+        //    return accountDetails;
+        //}
 
         public IActionResult Withdraw()
         {
@@ -141,48 +142,52 @@ namespace WebApplication1.Controllers
 
         public IActionResult TransactionHistory()
         {
-            // Retrieve transaction history for the hardcoded account_id 1
-            List<Transaction> transactionHistory = GetTransactionHistoryForAccount1();
+            return View();
+            //// Retrieve transaction history for the hardcoded account_id 1
+            //List<Transaction> transactionHistory = GetTransactionHistoryForAccount1();
 
-            // Pass the transaction history to the view
-            return View(transactionHistory);
+            //// Pass the transaction history to the view
+            //return View(transactionHistory);
         }
 
-        private List<Transaction> GetTransactionHistoryForAccount1()
-        {
-            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=OCBC;Integrated Security=True;";
 
-            List<Transaction> transactionHistory = new List<Transaction>();
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
+        //private List<Transaction> GetTransactionHistoryForAccount1()
 
-                // Query transaction history specifically for account_id 1
-                string query = "SELECT [transaction_id], [amount], [date], [category_id], [category], [account_id] FROM [OCBC].[dbo].[transactions] WHERE [account_id] = 1";
+        //{
+            
+            //        string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=OCBC;Integrated Security=True;";
 
-                using (SqlCommand command = new SqlCommand(query, connection))
-                {
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            Transaction transaction = new Transaction
-                            {
-                                TransactionID = Convert.ToInt32(reader["transaction_id"]),
-                                transactionamt = Convert.ToDecimal(reader["amount"]),
-                                transactiondate = Convert.ToDateTime(reader["date"]),
-                                categoryID = Convert.ToInt32(reader["category_id"]),
-                                transactioncat = Convert.ToString(reader["category"]),
-                                account_id = Convert.ToInt32(reader["account_id"]),
-                            };
+            //        List<Transaction> transactionHistory = new List<Transaction>();
+            //        using (SqlConnection connection = new SqlConnection(connectionString))
+            //        {
+            //            connection.Open();
 
-                            transactionHistory.Add(transaction);
-                        }
-                    }
-                }
-            }
+            //            // Query transaction history specifically for account_id 1
+            //            string query = "SELECT [transaction_id], [amount], [date], [category_id], [category], [account_id] FROM [OCBC].[dbo].[transactions] WHERE [account_id] = 1";
 
-            return transactionHistory;
+            //            using (SqlCommand command = new SqlCommand(query, connection))
+            //            {
+            //                using (SqlDataReader reader = command.ExecuteReader())
+            //                {
+            //                    while (reader.Read())
+            //                    {
+            //                        Transaction transaction = new Transaction
+            //                        {
+            //                            TransactionID = Convert.ToInt32(reader["transaction_id"]),
+            //                            transactionamt = Convert.ToDecimal(reader["amount"]),
+            //                            transactiondate = Convert.ToDateTime(reader["date"]),
+            //                            categoryID = Convert.ToInt32(reader["category_id"]),
+            //                            transactioncat = Convert.ToString(reader["category"]),
+            //                            account_id = Convert.ToInt32(reader["account_id"]),
+            //                        };
+
+            //                        transactionHistory.Add(transaction);
+            //                    }
+            //                }
+            //            }
+            //        }
+
+            //        return transactionHistory;
+            //    }
         }
     }
-}
